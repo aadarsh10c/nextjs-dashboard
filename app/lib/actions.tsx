0524e1961817973037afe016a8163d6e4790ed89
adarsh010c <adarsh010c@gmail.com>
2025-02-13 17:03:15 +0530

    Add error handling and delete functionality for invoices; implement error component and bind delete action

--- app/lib/actions.tsx
+++ app/lib/actions.tsx
@@ -25,33 +25,51 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error("Database Error:", error);
+  }
+
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 // Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
- 
- 
+
 export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
   });
- 
+
   const amountInCents = amount * 100;
- 
-  await sql`
+  try {
+    await sql`
     UPDATE invoices
     SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
     WHERE id = ${id}
   `;
- 
-  revalidatePath('/dashboard/invoices');
-  redirect('/dashboard/invoices');
+  } catch (error) {
+    console.error("Error while updating invoice:", error);
+  }
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+export async function deleteInvoice(id: string) {
+  throw new Error("Not implemented");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error("Error while deleting invoice:", error);
+  }
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
